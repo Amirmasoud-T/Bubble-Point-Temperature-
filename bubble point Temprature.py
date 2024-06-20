32031055750f5a1200c02ec69255b34cfbab2039
Amirmasoud-T <amirmasoudtavakol@gmail.com>
2024-06-21 02:35:40 +0330

    Update bubble point Temprature.py

--- bubble point Temprature.py
+++ bubble point Temprature.py
@@ -1,47 +1,56 @@
 #calculating bubble point temperature with antoine equationn using python
 import math
-import numpy as np
-quantity = int(input('number of components : '))
-x_value = np.array([])
+import numpy as np  
+import pandas as pd    #import necessary libraries
+quantity = int(input('number of components : ')) #number of your liquid components
+x_value = np.array([]) #value of youre liquid components 
 for i in range(quantity):
     x = float(input(f'x{i+1} = '))
-    x_value =  np.append(x_value,x)
-P = float(input('Enter the Pressure : '))
+    x_value =  np.append(x_value,x) #add value of liquid components to your array that define in line 6
+P = float(input('Enter the Pressure : ')) #get total pressure
 print(f'pressure is {P} kpa')
 A = np.zeros(quantity)
 B = np.zeros(quantity)
-C = np.zeros(quantity)
-for i in range(quantity):
+C = np.zeros(quantity) #A,B,C is antoine equation constants
+for i in range(quantity): #use for loop to returns antoine equation constants
     A[i] = float(input(f'A{i+1} is: '))
     B[i] = float(input(f'B{i+1} is: '))
     C[i] = float(input(f'C{i+1} is: '))
-Saturated_T = np.array([])
+saturation_T = np.array([]) #calculates saturation temperature from pressure
 for i in range(quantity): 
     TS = (B[i]/(A[i]-math.log(P))-C[i])
-    Saturated_T = np.append(Saturated_T,TS)
+    saturation_T = np.append( saturation_T,TS) #calculates saturation temperature for all components and add to array which is define in line 19
 for i in range(quantity):
-    print(f'T{i+1}S is {Saturated_T[i]}')
-T0 = np.sum(x_value*Saturated_T)
-print(f'selectiv component is 3 by defult & T0(first guss for temprature is {T0})')
-alphas = np.array([])
+    print(f'T{i+1}S is { saturation_T[i]}') #prints saturation temperature that calculated in before for loop
+T0 = np.sum(x_value* saturation_T) #calculates first Temperature
+Selective_component = int(input('selective component is : ')) #selective components that you want to calculate nexts stages by this component
+print(f'selective component (between 1,{quantity} ) is number {Selective_component}')
+alphas = []
 for i in range(quantity):
-    alpha = math.exp((A[i]-(B[i]/(T0+C[i])))-(A[quantity-1]-(B[quantity-1]/(T0+C[quantity-1]))))
-    alphas = np.append(alphas,alpha)
-for i in range(quantity):
-    print(f'alpha{i+1}3 = {alphas[i]}')
-P3S = (P/(np.sum(x_value*np.array([alphas]))))
-print(f'P{quantity}S is {P3S}')
-new_T = (B[quantity-1]/(A[quantity-1]-np.log(P3S)))-C[quantity-1]
-print(f'new temprature is {new_T}')
-err = abs(new_T - T0)
-my_T = []
-while err > 10**(-3):
-    new_T = (B[2]/(A[2]-np.log(P3S)))-C[2]
-    new_alphas = np.array([])
-    for i in range (quantity):
-        new_alpha = math.exp((A[i]-(B[i]/(new_T+C[i])))-(A[quantity-1]-(B[quantity-1]/(new_T+C[quantity-1]))))
-        new_alphas = np.append(new_alphas,new_alpha)
-    P3S = (80 / (np.sum(x_value * np.array([new_alphas]))))
-    new_T_2 = (B[quantity-1] / (A[quantity-1] - np.log(P3S))) - C[quantity-1]
-    err = abs(new_T_2 - new_T)
-print(F'bubble Point Temprature is {new_T} C')
+    alpha = np.exp((A[i]-(B[i]/(T0+C[i])))-(A[Selective_component-1]-(B[Selective_component-1]/(T0+C[Selective_component-1]))))
+    alphas.append(alpha.round(3)) #add alpha values to line 28 lists
+PS = (P/(np.sum(x_value*alphas))).round(3) #calculate selective components saturation pressures
+new_T_1 = ((B[Selective_component-1]/(A[Selective_component-1]-np.log(PS)))-C[Selective_component-1]).round(3) #calculats new T by selective components saturation pressures
+err = abs(new_T_1 - T0)
+my_T = [T0,new_T_1]
+PS_list = [PS]
+my_alphas = [alphas]
+while err>10**(-3):
+    new_T = (B[Selective_component-1]/(A[Selective_component-1]-np.log(PS)))-C[Selective_component-1]
+    new_alphas = []
+    for i in range(quantity):
+        new_alpha = (np.exp((A[i]-(B[i]/(new_T+C[i])))-(A[Selective_component-1]-(B[Selective_component-1]/(new_T+C[Selective_component-1]))))).round(4)
+        new_alphas.append(new_alpha.round(3))
+        my_alphas.append(new_alphas)
+    PS = (P/(np.sum(x_value * np.array([new_alphas])))).round(3)
+    PS_list.append(PS)
+    new_2_T = (B[Selective_component-1]/(A[Selective_component-1]-np.log(PS)))-C[Selective_component-1]
+    my_T.append((new_2_T).round(3))
+    err = abs(new_2_T - new_T)
+print(f'This is PS{Selective_component} list {PS_list}')
+print(f'This is Temprature list {my_T}')
+print(f'bubble point tempratuer is {(new_2_T).round(3)} C')
+Table = pd.DataFrame(my_alphas[0:len(PS_list)],columns=[f'alpha{i+1}{quantity}' for i in range(quantity)])
+Table[f'PS{Selective_component}'] = PS_list[0:len(PS_list)]
+Table['Temprature'] = my_T[0:(len(PS_list))]
+print(Table)
